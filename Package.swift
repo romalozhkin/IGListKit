03d13781f73cab2e0a6ebd62014a235eb01a50f7
Roman Lozhkin <r.lozhkin@tinkoff.ru>
2020-06-21 18:49:04 +0400

    fix

--- Package.swift
+++ Package.swift
@@ -1,15 +1,21 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "IGListKit",
+    platforms: [
+        .iOS(.v12),
+    ],
     products: [
         .library(name: "IGListKit", targets: ["IGListKit"])
     ],
     targets: [
         .target(
             name: "IGListKit",
-            path: "Source"
+            path: "Source",
+            cSettings: [
+                .headerSearchPath("./"),
+            ]
         )
     ]
 )
